1af850ba7d80aacd63ff950f8312f8d40848e715
12wollmana <awollman2@gmail.com>
2020-09-07 17:01:20 -0500

    Documentation

--- scrape/scrape_mars.py
+++ scrape/scrape_mars.py
@@ -8,36 +8,69 @@ sys.path.append("..")
 from config import chromedriver_path
 
 def scrape(exe_path = chromedriver_path):
+    """
+    Scrapes information from multiple websites
+    to get information on Mars.
+
+    Parameters
+    ----------
+    exe_path : str, optional
+            The executable path for a browser driver, by default is the chromedriver_path in config.py
+
+    Returns
+    -------
+    [dict]
+        Contains information on Mars.
+    """
     scraper = Mars_Scraper(exe_path)
     return scraper.scrape_all_data()
 
 class Mars_Scraper:
     def __init__(self, exe_path = chromedriver_path):
+        """
+        Initializes the Mars_Scraper class and opens a new browser session.
+
+        Parameters
+        ----------
+        exe_path : str, optional
+            The executable path for a browser driver, by default is the chromedriver_path in config.py
+        """
         self.exe_path = exe_path
         self.init_chrome_browser()
 
     def __del__(self):
+        """
+        On deconstruction, will close the browser if not already closed.
+        """
         if(self.browser is not None):
             self.close_browser()
     
     def init_chrome_browser(self):
         """
-        Opens a chrome session in splinter.
-        @ params:
-            exe_path - The path to a chrome browser driver, like chromedriver.
+        Opens a Chrome Browser using splinter.
         """
         executable_path = {"executable_path": self.exe_path}
         self.browser = Browser("chrome", **executable_path, headless = False)
 
     def close_browser(self):
+        """
+        Closes the browser.
+        """
         self.browser.quit()
     
     def get_soup(self, url):
         """
-        Opens a url and returns it's HTML BeautifulSoup object.
-        @ params:
-            browser - A splinter browser.
-            url - The url to get the HTML from.
+        Gets the HTML from a URL as a BeatifulSoup object.
+
+        Parameters
+        ----------
+        url : [str]
+            The url to get the html from.
+
+        Returns
+        -------
+         [BeatifulSoup]
+            This object contains the HTML.
         """
         browser = self.browser
         browser.visit(url)
@@ -47,6 +80,14 @@ class Mars_Scraper:
         return soup
     
     def get_latest_nasa_news(self):
+        """
+        Gets the latest nasa news article information.
+
+        Returns
+        -------
+        [dict]
+            Dictionary containing "title" and "paragraph" keys.
+        """
         browser = self.browser
 
         nasa_news_url = "https://mars.nasa.gov/news/"
@@ -68,6 +109,14 @@ class Mars_Scraper:
         return nasa_news_latest
 
     def get_mars_space_featured_image(self):
+        """
+        Gets the featured image from NASA JPL.
+
+        Returns
+        -------
+        [dict]
+            Dictionary containing "img_url" and "title" nodes.
+        """
         browser = self.browser
 
         jpl_base_url = "https://www.jpl.nasa.gov"
@@ -84,12 +133,28 @@ class Mars_Scraper:
         }
 
     def get_mars_facts_table_df(self):
+        """
+        Gets the planet profile facts for Mars.
+
+        Returns
+        -------
+        [DataFrame]
+            The table containing Mars facts.
+        """
         mars_facts_url = "https://space-facts.com/mars/"
         mars_facts_tables = pd.read_html(mars_facts_url)
         mars_planet_profile = mars_facts_tables[0]
         return mars_planet_profile
 
     def get_mars_facts_table_html(self):
+        """
+        Gets the planet profile facts for Mars in an HTML format.
+
+        Returns
+        -------
+        [str]
+            Contains the html for displaying Mars facts.
+        """
         mars_planet_profile = self.get_mars_facts_table_df()
 
         mars_planet_profile_html = mars_planet_profile.to_html(index=False, header=False)
@@ -97,6 +162,14 @@ class Mars_Scraper:
         return mars_planet_profile_html
 
     def get_mars_hemisphere_images(self):
+        """
+        Gets hemisphere images of Mars
+
+        Returns
+        -------
+        [list of dict]
+            Dictionaries containing "title" and "img_url" keys.
+        """
         browser = self.browser
 
         hemisphere_root = "https://astrogeology.usgs.gov"
@@ -122,6 +195,14 @@ class Mars_Scraper:
         return hemisphere_image_urls
 
     def scrape_all_data(self):
+        """
+        Runs through multiple website to get information on Mars.
+
+        Returns
+        -------
+        [dict]
+            Contains information on Mars.
+        """
         nasa_news = self.get_latest_nasa_news()
         featured_image = self.get_mars_space_featured_image()
         mars_facts_html = self.get_mars_facts_table_html()
